0b1392e2a4905d3e28eaccf5fb5b86a197c5763f
Your Name <you@example.com>
2023-03-16 11:21:20 -0600

    adding plugins for jate

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,38 @@ module.exports = () => {
       filename: "[name].bundle.js",
       path: path.resolve(__dirname, "dist"),
     },
-    plugins: [],
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Jate Editor",
+      }),
+
+      // Injects our custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Jate",
+        short_name: "Another text editor",
+        description: "Yet another text editor!",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
+    ],
 
     module: {
       rules: [
