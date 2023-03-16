7f750e809f28486da8753df591c1ac331a51009c
Your Name <you@example.com>
2023-03-16 11:40:33 -0600

    adding functionality

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,8 +27,8 @@ export const putDb = async (content) => {
 
   // Get confirmation of the request.
   const result = await request;
-  console.log("result.value", result);
-  return result;
+  // console.log("result.value", result);
+  // return result;
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -45,12 +45,12 @@ export const getDb = async () => {
   const store = tx.objectStore("jate");
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
   console.log("result.value", result);
-  return result;
+  return result?.value;
 };
 
 initdb();
